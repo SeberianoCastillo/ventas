95b07955bce6c70e591adfcbe9cc7c736596fefc
Seberiano Castillo <Seberiano Castillo@SEBERIANO>
2021-05-25 17:10:28 -0600

    Creando programa hola mundo

---
+++ Ventas/Ventas/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ventas
+{
+    public partial class frmHolaMundo : Form
+    {
+        public frmHolaMundo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnProcesar_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hola Mundo");
+        }
+    }
+}
