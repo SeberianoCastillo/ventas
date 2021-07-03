d35406f88ec491592c370ae6642b9c3eb7e3f118
SeberianoCastillo <seberiano.castillo@unah.hn>
2021-07-03 15:27:38 -0600

    Implemente las funcionalidades de Crear, Guardar, Eliminar a su mantenimiento

--- Citas Medicas/BL.CitasMedicas/CitasBL.cs
+++ Citas Medicas/BL.CitasMedicas/CitasBL.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.Linq;
-using System.Runtime.Remoting;
-using System.Runtime.Remoting.Messaging;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BL.CitasMedicas
 {
